5f6e2e9d9e3979322793e534c5cc051388acc374
francis-gomes-esq <figomesokoye@protonmail.com>
2024-01-21 23:18:27 +0000

    add logic to display current day, time block functions, and load local storage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
+$(document).ready(function () {
+	$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'))
 
+	function updateColors() {
+		var currentHour = dayjs().hour()
+		console.log('currentHour: ' + currentHour)
+		$('.time-block').each(function () {
+			var blockHour = parseInt(
+				$(this).children('textarea').attr('data-hour')
+			)
+			var hour = $(this).find('textarea').attr('data-hour')
+			var event = localStorage.getItem('event_' + hour)
+
+			if (event) {
+				$(this).find('textarea').val(event)
+			}
+
+			if (blockHour < currentHour) {
+				$(this).addClass('past')
+			} else if (blockHour === currentHour) {
+				$(this).removeClass('past')
+				$(this).addClass('present')
+			} else {
+				$(this).removeClass('past')
+				$(this).removeClass('present')
+				$(this).addClass('future')
+			}
+		})
+	}
+})
