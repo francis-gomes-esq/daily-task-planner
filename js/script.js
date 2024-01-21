cd2ed5d793b4e28d51f8cc9a71dd79c632a8349e
francis-gomes-esq <figomesokoye@protonmail.com>
2024-01-21 23:23:27 +0000

    add comments for: daily task planner, timeblock function colours, local storage load events

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+	// Display the current day at the top of the daily-task-planner
 	$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'))
 
+	// Function to update timeblock colors based on past, present, and future
 	function updateColors() {
 		var currentHour = dayjs().hour()
 		console.log('currentHour: ' + currentHour)
@@ -11,16 +13,20 @@ $(document).ready(function () {
 			var hour = $(this).find('textarea').attr('data-hour')
 			var event = localStorage.getItem('event_' + hour)
 
+			// Load event from local storage, if present
 			if (event) {
 				$(this).find('textarea').val(event)
 			}
 
 			if (blockHour < currentHour) {
+				// Past time
 				$(this).addClass('past')
 			} else if (blockHour === currentHour) {
+				// Present time
 				$(this).removeClass('past')
 				$(this).addClass('present')
 			} else {
+				// Future time
 				$(this).removeClass('past')
 				$(this).removeClass('present')
 				$(this).addClass('future')
